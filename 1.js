753c5e2cd10600204c8ff094fac0ab3662a6a111
Starlend <posohovrom@icloud.com>
2020-07-06 14:18:07 +0300

    added CSS styles

--- 1.js
+++ 1.js
@@ -6,10 +6,38 @@ var preloader = document.getElementById('icon');
 preloader.classList.add("hide");
 
 //обработчик клика по кнопке
-$("#buttonStart").click(function (event) {
+/*$("#buttonStart").click(function (event) {
     preloader.classList.remove("hide");
     setTimeout(function() {
         preloader.classList.add("hide");
         window.open('secondpage.html');
     }, 5000);
-    });
+    });*/
+
+	var preloader = document.getElementById('icon');
+
+	// нет необходимости если ты скроешь ее прямо в html'е
+	// скрываю иконку вначале
+	// preloader.classList.add("hide");
+	
+	//обработчик клика по кнопке
+	$("#buttonStart").click(function (event) {
+		preloader.classList.remove("hide");
+	
+		new Promise(function(resolve, reject)
+		{
+			setTimeout(() => {
+				console.log("async request")
+				for (var i = 0; i < 100000; i++) {
+					// логирую в консоль для прерывания
+					console.log(i);
+				}
+				resolve(1);
+			})
+		}).then(function() {
+			window.open('secondpage.html');
+		  	console.log("Going to the next page"); 
+		});
+		
+		console.log("Button click is finished");
+	});
